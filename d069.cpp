298dc3130f204f0759c74e768d58032c6e6c1bf6
Noah Tseng <sam123.tseng@gmail.com>
2026-04-27 17:59:59 +0800

    solve: d069

---
+++ d069.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (void){
+ios::sync_with_stdio(0);
+cin.tie(0);
+int n,y;
+cin>>n;
+for(int i=1;i<=n;i++){
+        cin>>y;
+        if((y%4==0&&y%100!=0)||y%400==0){
+                cout<<"a leap year\n";
+        }
+        else{
+                cout<<"a normal year\n";
+        }
+
+}
+return 0 ;
+
+}
