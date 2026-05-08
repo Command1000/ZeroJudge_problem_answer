60acb7f183ac9c1496213af0920e23cfabb5b260
Noah Tseng <sam123.tseng@gmail.com>
2026-05-08 16:43:44 +0800

    solve: f345

---
+++ f345.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+vector<int> v;
+int n,x;
+cin>>n;
+for(int i=0;i<n;i++){
+	cin>>x;
+	v.push_back(x);
+}
+for(int i=n-1;i>=0;i--){
+	cout<<v[i]<<" ";
+}
+	return 0;
+}
